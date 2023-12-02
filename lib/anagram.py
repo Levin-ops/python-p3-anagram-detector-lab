732fbc9233e69f6f649682ebf3cb2f84a7f635da
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-02 03:19:55 +0300

    All tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -5,8 +5,8 @@ class Anagram:
         self.word = word
 
     def match(self, input_list):
-        matched_words = [i for i in input_list if sorted(i.lower()) == sorted(self.word.lower()) and i.lower() != self.word.lower()]
-        return matched_words
+        matching_words = [i for i in input_list if sorted(i.upper()) == sorted(self.word.upper()) and i.lower() != self.word.lower()]
+        return matching_words
 
 
 
