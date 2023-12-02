6af363a61fce91abd2ebba64996d9387f18ee716
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-02 03:18:02 +0300

    Assignment Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, input_list):
+        matched_words = [i for i in input_list if sorted(i.lower()) == sorted(self.word.lower()) and i.lower() != self.word.lower()]
+        return matched_words
+
+
+
